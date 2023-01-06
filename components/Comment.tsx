51075143b222f5250de22438089272dfcad07e63
prefer2 <sunho6200@gmail.com>
2023-01-06 15:42:35 +0900

    feat: 댓글 컴포넌트 수정

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,20 +1,23 @@
 import { useEffect, useRef } from "react";
 
 const Comment = () => {
-  const commentsEl = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const scriptEl = document.createElement("script");
-    scriptEl.async = true;
-    scriptEl.src = "https://utteranc.es/client.js";
-    scriptEl.setAttribute("repo", "prefer2/prefer2.dev");
-    scriptEl.setAttribute("issue-term", "title");
-    scriptEl.setAttribute("theme", `github-light`);
-    scriptEl.setAttribute("crossorigin", "anonymous");
-    commentsEl.current?.appendChild(scriptEl);
-  }, []);
-
-  return <div ref={commentsEl} />;
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem) {
+          return;
+        }
+        const scriptElem = document.createElement("script");
+        scriptElem.src = "https://utteranc.es/client.js";
+        scriptElem.async = true;
+        scriptElem.setAttribute("repo", "prefer2/prefer2.dev");
+        scriptElem.setAttribute("issue-term", "pathname");
+        scriptElem.setAttribute("theme", "github-light");
+        scriptElem.crossOrigin = "anonymous";
+        elem.replaceChildren(scriptElem);
+      }}
+    />
+  );
 };
 
 export default Comment;
