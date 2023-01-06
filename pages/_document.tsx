48498ee5ffde8097549be24293d12e94f9ba59a9
prefer2 <sunho6200@gmail.com>
2023-01-06 14:51:58 +0900

    feat: google site verification 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="GHi1z9zJi8Fxs-W5helbwbS0bASwpLfk2LRf19XtR9o"
+        />
+      </Head>
       <body className="dark:bg-code-dark dark:text-white">
         <Main />
         <NextScript />
