096afda03f1ceb78864dd6e6f1bcfa7ebee21896
prefer2 <sunho6200@gmail.com>
2023-01-26 13:06:46 +0900

    feat: naver search meta tag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
           name="google-site-verification"
           content="GHi1z9zJi8Fxs-W5helbwbS0bASwpLfk2LRf19XtR9o"
         />
+        <meta
+          name="naver-site-verification"
+          content="5af7847b4ec64c700ffb067ffe8fbdf1e9ced8da"
+        />
       </Head>
       <body className="dark:bg-code-dark dark:text-white">
         <Main />
